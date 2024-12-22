91a2ea1b4ddff2406933762b295208a20d46d75e
devfadyfathy <fadyfathymaher3@gmail.com>
2024-12-22 19:28:08 +0200

    edit in char

--- 13-Basic-Mathematical-Operations.js
+++ 13-Basic-Mathematical-Operations.js
@@ -13,5 +13,5 @@ function basicOp(operation, value1, value2) {
   if (operation === "+") return value1 + value2;
   if (operation === "-") return value1 - value2;
   if (operation === "*") return value1 * value2;
-  if (operation === "/") return value / value2;
+  if (operation === "/") return value1 / value2;
 }
